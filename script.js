34ad31778f56a860bf6e4ecdfa5c339d58566ab7
ElanekJ <eliasjozef01@gmail.com>
2023-01-24 23:44:12 +0000

    creating script.js, trying to get work getItem for every hour

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-/*$("button").on("click", function(event){
-
-    if(localStorage.getItem("9a") !== null)
-    {
-      localStorage.clear("9a")
-               
-   }
-
-    else{         
-      localStorage.setItem("9a", $("textarea").val())
-      console.log(localStorage.getItem("9a"))
-    }
-  })
-*/
-
+let identEl = $()
 $("button").on("click", function(event){
 
     console.log($(event.target).siblings("textarea").val())
     let dataTimeEl = $(event.target).attr("data-time");
-    localStorage.setItem(dataTimeEl, $("textarea").val());
+    localStorage.setItem(dataTimeEl, $(event.target).siblings("textarea").val());
+    console.log()
     $(event.target).siblings("textarea").text(localStorage.getItem(dataTimeEl));
 })
 
-    
+
+for (let i = 0; i < $("textarea", ".container").length; i++) {
   
-  $('#currentDay').html(moment().format("dddd, MMMM Do YYYY"))
+}
+
+$('#currentDay').html(moment().format("dddd, MMMM Do YYYY"))
   let time = moment().format("H")
 
-  console.log(time)
-  
 
-  if(parseInt($("button").attr("data-time")) < parseInt(time))
+  function setScheduler(hour){
+
+  if(parseInt(hour) < parseInt(time))
   {
-    console.log(time)
- //   $("textarea").addClass("future")
+    $("button").siblings("textarea").addClass("past");
   }
+  else if(parseInt($("button").attr("data-time")) === parseInt(time)){
+    $("button").attr("data-time",([i+8])).siblings("textarea").addClass("present");
+    
+  }
+  else{
+    $("button").attr("data-time",([i+8])).siblings("textarea").addClass("future");
+  }
+
+$("#8am").text(localStorage.getItem("8"))
+$("#9am").text(localStorage.getItem("9"))
+$("#10am").text(localStorage.getItem("10"))
+$("#11am").text(localStorage.getItem("11"))
+$("#12pm").text(localStorage.getItem("12"))
+$("#1pm").text(localStorage.getItem("13"))
+$("#2pm").text(localStorage.getItem("14"))
+$("#3pm").text(localStorage.getItem("15"))
+$("#4pm").text(localStorage.getItem("16"))
+$("#5pm").text(localStorage.getItem("17"))
+  
+}
+
+
+
+  
